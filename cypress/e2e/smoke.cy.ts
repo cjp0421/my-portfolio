491499a631951291e0ed91d5605b5d06ec3978f5
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2025-11-07 13:54:34 -0600

    added base url for cypress config and refactored test to use it appropriately

--- cypress/e2e/smoke.cy.ts
+++ cypress/e2e/smoke.cy.ts
@@ -4,7 +4,7 @@
 describe('Portfolio site', () => {
     it('loads the home page and shows the Hero section', () => {
         // cy.visit('https://cjp0421.github.io/my-portfolio')
-        cy.visit('http://localhost:5173')
+        cy.visit('/')
         cy.contains('Carol Pedersen').should('be.visible')
     })
 })
