3f4e7bd83cf32e595d327428c0c064a865ad2ce5
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2025-11-08 16:17:22 -0600

    added basic different viewport tests for navbar

--- cypress/e2e/navigation.cy.ts
+++ cypress/e2e/navigation.cy.ts
@@ -1,5 +1,7 @@
         /// <reference types="cypress" />
 
+const navItems = ['Home', 'Capabilities', 'Projects', 'Contact'];
+
 describe('AppBar navigation', () => {
     it('stays visible and jumps to the capabilities section when clicked', () => {
         cy.visit("/")
@@ -17,7 +19,7 @@ describe('AppBar navigation', () => {
 
         cy.get('header').should('be.visible');
     })
-        it('jumps to the projects, contact, and home sections when clicked', () => {
+    it('jumps to the projects, contact, and home sections when clicked', () => {
         cy.visit("/")
 
         cy.get('nav').contains('Projects').click();
@@ -37,5 +39,23 @@ describe('AppBar navigation', () => {
         cy.url().should('include', '#Home');
 
         cy.get('#Home').should('be.visible');
+    })
+    it('shows inline nav items and hides menu icon on desktop', () => {
+        cy.visit('/');
+        cy.viewport('macbook-15');
+        cy.get('button[aria-label="open drawer"]').should('not.be.visible');
+
+        navItems.forEach((item) => {
+          cy.get('header').contains(item).should('be.visible');
+        })
+    })
+    it('shows menu icon and hides inline nav items on mobile', () => {
+      cy.visit('/');
+      cy.viewport('iphone-6');
+      cy.get('button[aria-label="open drawer"]').should('be.visible')
+
+      navItems.forEach((item) => {
+        cy.get('header').contains(item).should('not.be.visible');
+      })
     })
 })
