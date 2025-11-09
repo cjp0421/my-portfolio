c8aaf61303df02e308cef5a17c7a6d68fe877149
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2025-11-09 11:29:03 -0600

    added test/implementation for mobile navigation drawer opening/closing + linting changes

--- cypress/e2e/navigation.cy.ts
+++ cypress/e2e/navigation.cy.ts
@@ -1,61 +1,82 @@
-        /// <reference types="cypress" />
+/// <reference types="cypress" />
 
 const navItems = ['Home', 'Capabilities', 'Projects', 'Contact'];
 
 describe('AppBar navigation', () => {
-    it('stays visible and jumps to the capabilities section when clicked', () => {
-        cy.visit("/")
+  it('stays visible and jumps to the capabilities section when clicked', () => {
+    cy.visit("/")
 
-        cy.get('header')
-          .should('be.visible')
-          .and('have.css', 'position')
-          .and('match', /static/);
+    cy.get('header')
+      .should('be.visible')
+      .and('have.css', 'position')
+      .and('match', /static/);
 
-        cy.contains('Capabilities').click();
+    cy.contains('Capabilities').click();
 
-        cy.url().should('include', '#Capabilities');
+    cy.url().should('include', '#Capabilities');
 
-        cy.get('#Capabilities').should('be.visible');
+    cy.get('#Capabilities').should('be.visible');
 
-        cy.get('header').should('be.visible');
+    cy.get('header').should('be.visible');
+  })
+  it('jumps to the projects, contact, and home sections when clicked', () => {
+    cy.visit("/")
+
+    cy.get('nav').contains('Projects').click();
+
+    cy.url().should('include', '#Projects');
+
+    cy.get('#Projects').should('be.visible');
+
+    cy.get('nav').contains('Contact').click();
+
+    cy.url().should('include', '#Contact');
+
+    cy.get('#Contact').should('be.visible');
+
+    cy.get('nav').contains('Home').click();
+
+    cy.url().should('include', '#Home');
+
+    cy.get('#Home').should('be.visible');
+  })
+  it('shows inline nav items and hides menu icon on desktop', () => {
+    cy.viewport('macbook-15');
+    cy.visit('/');
+
+    cy.get('button[aria-label="open drawer"]').should('not.be.visible');
+
+    navItems.forEach((item) => {
+      cy.get('header').contains(item).should('be.visible');
     })
-    it('jumps to the projects, contact, and home sections when clicked', () => {
-        cy.visit("/")
+  })
+  it('shows menu icon and hides inline nav items on mobile', () => {
+    cy.viewport('iphone-6');
+    cy.visit('/');
+    cy.get('button[aria-label="open drawer"]').should('be.visible')
 
-        cy.get('nav').contains('Projects').click();
-
-        cy.url().should('include', '#Projects');
-
-        cy.get('#Projects').should('be.visible');
-
-        cy.get('nav').contains('Contact').click();
-
-        cy.url().should('include', '#Contact');
-
-        cy.get('#Contact').should('be.visible');
-
-        cy.get('nav').contains('Home').click();
-
-        cy.url().should('include', '#Home');
-
-        cy.get('#Home').should('be.visible');
+    navItems.forEach((item) => {
+      cy.get('header').contains(item).should('not.be.visible');
     })
-    it('shows inline nav items and hides menu icon on desktop', () => {
-        cy.visit('/');
-        cy.viewport('macbook-15');
-        cy.get('button[aria-label="open drawer"]').should('not.be.visible');
+  })
+  it('opens/closes the mobile drawer when the menu icon is clicked', () => {
+    cy.viewport('iphone-6');
+    cy.visit('/');
 
-        navItems.forEach((item) => {
-          cy.get('header').contains(item).should('be.visible');
-        })
-    })
-    it('shows menu icon and hides inline nav items on mobile', () => {
-      cy.visit('/');
-      cy.viewport('iphone-6');
-      cy.get('button[aria-label="open drawer"]').should('be.visible')
+    cy.get('button[aria-label="open drawer"]').click();
 
-      navItems.forEach((item) => {
-        cy.get('header').contains(item).should('not.be.visible');
-      })
+    cy.get('h6').contains('Carol Pedersen').should('be.visible');
+
+    navItems.forEach((item) => {
+      cy.get('[aria-label="mobile navigation"]').contains(item).should('be.visible')
+
     })
+
+    cy.get('body').click('topRight');
+
+    navItems.forEach((item) => {
+      cy.get('[aria-label="mobile navigation"]').contains(item).should('not.be.visible')
+
+    })
+  })
 })
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,23 +1,48 @@
-import { AppBar, Toolbar, IconButton, Typography, Box, Button } from "@mui/material";
+import { AppBar, Toolbar, IconButton, Typography, Box, Button, Divider, List, ListItem, ListItemButton, ListItemText, Drawer } from "@mui/material";
 import MenuIcon from '@mui/icons-material/Menu';
+import { useState } from "react";
 
 const navItems = ['Home', 'Capabilities', 'Projects', 'Contact'];
 
 export default function NavBar() {
-    return (
-        <header>
-        {/*https://mui.com/material-ui/react-app-bar/#responsive-app-bar-with-drawer*/}
-        <AppBar component="nav">
-          <Toolbar
+  const [mobileOpen, setMobileOpen] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+      <Typography variant="h6" sx={{ m: 2 }}>
+        Carol Pedersen
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton sx={{ textAlign: 'center' }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <header>
+      {/*https://mui.com/material-ui/react-app-bar/#responsive-app-bar-with-drawer*/}
+      <AppBar component="nav">
+        <Toolbar
           sx={{
             backgroundColor: '#000'
           }}
-          >
-            <IconButton
+        >
+          <IconButton
             color="inherit"
             aria-label="open drawer"
             edge="start"
-            // onClick={handleDrawerToggle}
+            onClick={handleDrawerToggle}
             sx={{ mr: 2, display: { sm: 'none' } }}
           >
             <MenuIcon />
@@ -29,15 +54,32 @@ export default function NavBar() {
           >
             Carol Pedersen
           </Typography>
-            <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
             {navItems.map((item) => (
               <Button href={`#${item}`} key={item} sx={{ color: '#fff' }}>
                 {item}
               </Button>
             ))}
           </Box>
-          </Toolbar>
-        </AppBar>
-      </header>
-    )
+        </Toolbar>
+      </AppBar>
+      <nav aria-label="mobile navigation">
+        <Drawer
+          variant="temporary"
+          aria-label="mobile navigation"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true,
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: "150px" },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </nav>
+    </header>
+  )
 }
